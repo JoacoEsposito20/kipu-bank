c7a4e2c096f9e91830543efcfdc58139df1b19b4
JoacoEsposito <espositojoaquin20@gmail.com>
2025-09-26 18:07:32 -0300

    Deposito

--- contracts/kipu-bank.sol
+++ contracts/kipu-bank.sol
@@ -7,67 +7,55 @@ pragma solidity 0.8.30;
 @notice Este contrato representa un banco personal.
 */
 contract KipuBank{
-    uint256 private s_bovedaPersonal = 0; //Variable que representa la boveda personal
-    uint256 private immutable s_umbralTransaccion = 10; //Constante que define el maximo por transaccion
-    uint256 private immutable s_bankCap; //Constante que determina el maximo de deposito. Se define en tiempo de deploy. 
+    mapping (address usuario => uint256 saldo) private s_usuarios; //Mapa que almacena los usuarios con su saldo
+    uint256 private immutable i_maxTransaccion = 1000; //Determina el maximo por transaccion
+    uint256 private immutable i_bankCap; //Determina el maximo de deposito en el banco
+    uint256 private nroDepositos;
+    uint256 private nroRetiros;
 
-    event KipuBank_montoDepositado(string mensaje);
-    event KipuBank_montoRetirado(string mensaje);
+    //@notice Evento que se lanza cuando se realiza correctamente un deposito.
+    event KipuBank_DepositoRealizado(uint256);
+    //@notice Evento que se lanza cuando se realiza correctamente un retiro.
+    event KipuBank_RetiroRealizado(uint256);
 
-    /*
-    @notice Funcion para depositar saldo.
-    @param _monto es el monto que se quiere depositar.
-    */
-    function depositar(uint256 _monto) external {
-        
-        if(_monto < s_bankCap && _monto > 0){
-            s_bovedaPersonal = s_bovedaPersonal + _monto;
-            emit KipuBank_montoDepositado("Monto depositado");
-        }else{
-            //¿Cargar umbral y aclarar que no se cargo todo o no cargar?
-        }
+    // Errors
+    error KipuBank_MontoDeDepositoExcedido(uint256);
+    error KipuBank_DepositoInvalido(uint256);
+    error KipuBank_MontoTransaccionExcedido(uint256,uint256);
+
+    // Modifiers
+    modifier validarDeposito(uint256 _monto){
+        uint256 balance = address(this).balance;
+        if(_monto <= 0) revert KipuBank_DepositoInvalido(_monto); 
+        if(_monto+balance > i_bankCap) revert KipuBank_MontoDeDepositoExcedido(balance);
+        if(_monto > i_maxTransaccion) revert KipuBank_MontoTransaccionExcedido(_monto,i_maxTransaccion);
+        _;
+    }   
+
+
+    // Functions
+    function depositar()external payable validarDeposito(msg.value){
+        s_usuarios[msg.sender] += msg.value;
+        emit KipuBank_DepositoRealizado(msg.value);
     }
 
-    /*
-    @notice Funcion para retirar saldo.
-    @param _monto es el monto que se quiere retirar.
-    */
-    function retirar(uint256 _monto) external {
-        if(validarRetiro(_monto)){
-            s_bovedaPersonal = s_bovedaPersonal - _monto;
-            emit KipuBank_montoRetirado("Monto retirado");
-        }
+
+    // Constructor
+    constructor(uint256 _montoBankCap){
+        i_bankCap = _montoBankCap;  
+        nroDepositos = 0;
+        nroRetiros = 0;
     }
 
-    /*
-    @notice Funcion para consultar el saldo en la boveda.
-    @return monto_ es el monto disponible.
-    */
-    function consultarSaldo() external view returns (uint256 monto_){
-        monto_ = s_bovedaPersonal;
-    }
+    // Receive & Fallback
+    receive() external payable { }
+    fallback() external{}
 
-    /*
-    @notice Funcion para validar retiro.
-    @param _monto es el monto que se quiere retirar.
-    @return es_valido_ es la validacion.
-    */
-    function validarRetiro(uint256 _monto) private view returns (bool es_valido_){
-        if(_monto <= s_umbralTransaccion && _monto <= s_bovedaPersonal && _monto > 0){
-            return true;
-        }else{
-            if(_monto <= 0){
-                //ERROR: monto no valido
-                return false;
-            }else{
-                if(_monto >= s_umbralTransaccion){
-                    //ERROR: Se sobrepasa el umbral de transaccion
-                    return false;
-                }else{
-                    //ERROR: No hay saldo suficiente
-                    return false;
-                }
-            }
-        }
-    }
+    // External
+    // Public
+    // Internal
+    // Private
+    // View & Pure  
+
+
 }
